02d8d1e6ac2f7a3cb9237cad48db642e7c661d25
DiegoB <diegobolivar2004@gmail.com>
2022-10-01 16:14:37 -0400

    primera subida de datos al repositorio

---
+++ hola.py
@@ -0,0 +1,8 @@
+operacion = input(str('Introduzca su operacion: '))
+operacion = list[]
+operacion.split('+')
+operacion.split('-')
+operacion.split('*')
+operacion.split('/')
+for i in operacion:
+    
